d18c35eb1e845c9da711941f1428ec7555d43bb5
John Newman <rescuecreative@gmail.com>
2017-04-13 11:30:20 -0600

    upgrade react

--- frontend/src/js/components/App.js
+++ frontend/src/js/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class App extends Component {
   constructor() {
@@ -16,9 +17,9 @@ class App extends Component {
 }
 
 App.propTypes = {
-  data: React.PropTypes.object.isRequired,
-  actions: React.PropTypes.object.isRequired,
-  handlers: React.PropTypes.object.isRequired
+  data: PropTypes.object.isRequired,
+  actions: PropTypes.object.isRequired,
+  handlers: PropTypes.object.isRequired
 };
 
 export default App;
